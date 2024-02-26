6948dac1d17ff76fdd52f64b02e3836245745ded
MaksGavriliuk <maks.revival27@gmail.com>
2024-02-27 01:44:08 +0300

    refactor: change docker port

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,20 @@
+CREATE TABLE specialties
+(
+    id   BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE groups
+(
+    id                BIGSERIAL PRIMARY KEY,
+    group_number      VARCHAR(255) NOT NULL,
+    specialization_id INT          NOT NULL,
+    FOREIGN KEY (specialization_id) REFERENCES specialties (id)
+);
+
 CREATE TABLE students
 (
-    id            BIGSERIAL INT PRIMARY KEY,
+    id            BIGSERIAL PRIMARY KEY,
     surname       VARCHAR(255) NOT NULL,
     name          VARCHAR(255) NOT NULL,
     patronymic    VARCHAR(255) NOT NULL,
@@ -12,7 +26,7 @@ CREATE TABLE students
 
 CREATE TABLE student_activities
 (
-    id            BIGSERIAL INT PRIMARY KEY,
+    id            BIGSERIAL PRIMARY KEY,
     description   VARCHAR(255) NOT NULL,
     activity_date DATE         NOT NULL,
     student_id    INT          NOT NULL,
@@ -21,7 +35,7 @@ CREATE TABLE student_activities
 
 CREATE TABLE scientific_publications
 (
-    id                BIGSERIAL INT PRIMARY KEY,
+    id                BIGSERIAL PRIMARY KEY,
     journal           VARCHAR(255) NOT NULL,
     publication_title VARCHAR(255) NOT NULL,
     publication_date  DATE         NOT NULL,
@@ -29,16 +43,3 @@ CREATE TABLE scientific_publications
     FOREIGN KEY (student_id) REFERENCES students (id)
 );
 
-CREATE TABLE specialties
-(
-    id   BIGSERIAL INT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE groups
-(
-    id                BIGSERIAL INT PRIMARY KEY,
-    group_number      VARCHAR(255) NOT NULL,
-    specialization_id INT          NOT NULL,
-    FOREIGN KEY (specialization_id) REFERENCES specialties (id)
-);
