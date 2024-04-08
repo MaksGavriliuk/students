6d4f1d1d5fe372161a775222b0b96b963fd6e974
MaksGavriliuk <maks.revival27@gmail.com>
2024-04-09 02:35:06 +0300

    create: FileUploadConfiguration

---
+++ src/main/java/org/example/students/config/FileUploadConfiguration.java
@@ -0,0 +1,21 @@
+package org.example.students.config;
+
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+
+@Configuration
+public class FileUploadConfiguration {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.parse("50MB"));
+        factory.setMaxRequestSize(DataSize.parse("50MB"));
+        return factory.createMultipartConfig();
+    }
+
+}
